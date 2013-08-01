6326cd1bb0d867d30d1a3ec5c80e2f810aa1a3c7
Robert Gorecki <robertgorecki86@gmail.com>
2013-08-01 21:55:17 +0100

    added opacity controller

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 var cube = document.querySelector('.cube'),
-	links = document.querySelectorAll('.links a');
+	links = document.querySelectorAll('.links a'),
+	walls = document.querySelectorAll('.cube li'),
+	opacityCtrl = document.querySelector('.opacity-control');
 
 Array.prototype.forEach.call(links, function(link) {
 	link.addEventListener("click", function(e) {
 		e.preventDefault();
-		console.log(e.target.getAttribute("href"));
 		cube.setAttribute("data-visible", e.target.getAttribute("href"));
+	});
+});
+
+opacityCtrl.addEventListener("change", function(e) {
+	e.preventDefault();
+	Array.prototype.forEach.call(walls, function(wall) {
+		wall.setAttribute("style", "opacity:" + e.target.value + ";");
 	});
 });
